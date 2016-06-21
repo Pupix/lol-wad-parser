3f010f535554746c92e638e6f2c7c1f74f18f31f
Pupix <manolea.robert@gmail.com>
2016-06-21 23:35:20 +0200

    Dependency bump

---
+++ index.js
@@ -0,0 +1,8 @@
+/*jslint browser: true, devel: true, node: true, ass: true, nomen: true, unparam: true, indent: 4 */
+
+(function () {
+    "use strict";
+
+    module.exports = require('./lib/index');
+
+}());
